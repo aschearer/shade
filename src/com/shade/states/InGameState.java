ec2f49a8095d43c44e15402d45d8be5b537f8548
Alex Schearer <aschearer@gmail.com>
2008-11-17 18:35:47 -0800

    restoring playing field

--- src/com/shade/states/InGameState.java
+++ src/com/shade/states/InGameState.java
@@ -11,7 +11,9 @@ import com.shade.base.Level;
 import com.shade.crash.CrashLevel;
 import com.shade.entities.Block;
 import com.shade.entities.Dome;
+import com.shade.entities.Fence;
 import com.shade.entities.Player;
+import com.shade.entities.util.MushroomFactory;
 import com.shade.lighting.GlobalLight;
 import com.shade.lighting.LightMask;
 import com.shade.lighting.LightSourceProxy;
@@ -25,6 +27,7 @@ public class InGameState extends BasicGameState {
     private LightMask view;
     private Level<LuminousEntity> model;
     private LightSourceProxy lights;
+    private MushroomFactory factory;
 
     @Override
     public int getID() {
@@ -34,7 +37,7 @@ public class InGameState extends BasicGameState {
     public void init(GameContainer container, StateBasedGame game)
     throws SlickException {
         lights = new LightSourceProxy();
-        lights.add(new GlobalLight(18, (float) (4 * Math.PI / 3)));
+        lights.add(new GlobalLight(12, (float) (4 * Math.PI / 3)));
 
         view = new LightMask(6);
         view.add(lights);
@@ -42,10 +45,31 @@ public class InGameState extends BasicGameState {
 
         model = new CrashLevel<LuminousEntity>(8, 6, 100);
         model.add(new Player(300, 200));
-        model.add(new Block(250, 350, 100, 100, 6));
-        model.add(new Block(400, 100, 150, 150, 9));
-        model.add(new Block(450, 300, 125, 100, 7));
-        model.add(new Dome(150, 150, 50, 7));
+        //model.add(new Block(250, 350, 100, 100, 6));
+        //model.add(new Block(400, 100, 150, 150, 9));
+        //model.add(new Block(450, 300, 125, 100, 7));
+        //model.add(new Dome(150, 150, 50, 7));
+        //model.add(new Fence(100, 50, 80, 20, 6));
+        model.add(new Block(55, 355, 125, 125, 16));
+        model.add(new Block(224, 424, 56, 56, 6));
+        model.add(new Block(324, 424, 56, 56, 6));
+        model.add(new Block(75, 225, 56, 56, 6));
+        model.add(new Block(545, 330, 80, 80, 10));
+        model.add(new Block(445, 460, 80, 80, 10));
+        // domes
+        model.add(new Dome(288, 165, 32, 7));
+        model.add(new Dome(180, 95, 44, 10));
+        model.add(new Dome(300, 65, 25, 6));
+        model.add(new Dome(710, 80, 28, 6));
+        model.add(new Dome(600, 100, 40, 9));
+        model.add(new Dome(680, 220, 60, 13));
+        // fences
+        model.add(new Fence(250, 250, 11, 120, 5));
+        model.add(new Fence(390, 140, 120, 11, 5));
+        model.add(new Fence(715, 368, 11, 120, 5));
+        model.add(new Fence(50, 50, 11, 120, 5));
+
+        factory = new MushroomFactory(8, .01);
     }
 
     public void render(GameContainer container, StateBasedGame game, Graphics g)
@@ -57,6 +81,9 @@ public class InGameState extends BasicGameState {
     throws SlickException {
         model.update(game, delta);
         lights.update(game, delta);
+        if (factory.active()) {
+            model.add(factory.getMushroom(container));
+        }
     }
 
 }
