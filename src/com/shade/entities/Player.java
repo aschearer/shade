0563f8ab06bcbacf18d2ba4f2b2e703b1b84a7d4
Alex Schearer <aschearer@gmail.com>
2008-12-06 09:03:45 -0800

    merging JJ's changing and fixing small bug

--- src/com/shade/entities/Player.java
+++ src/com/shade/entities/Player.java
@@ -40,7 +40,7 @@ public class Player extends Linkable {
     }
 
     private void initResources() throws SlickException {
-        normal = new Image("entities/player/player2.png");
+        normal = new Image("entities/player/player.png");
     }
 
     private void initStates() {
